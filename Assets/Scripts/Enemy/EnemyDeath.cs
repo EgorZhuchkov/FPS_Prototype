32159392652f7b26c6e75a5155f3712b097bd0d5
Egor <egorzhuchkov2013@yanex.ru>
2021-04-22 14:21:03 +0300

    Enemy dissolve animation

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -1,15 +1,24 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Enemy
 {
+  [RequireComponent(typeof(MeshRenderer))]
   [RequireComponent(typeof(EnemyHealth))]
   public class EnemyDeath : MonoBehaviour
   {
     public EnemyHealth health;
     public EnemyFollowPlayer follow;
     public float bodyDestructionDelay = 1.0f;
-    
+
+    private MeshRenderer _renderer;
+    private static readonly int DissolveAmount = Shader.PropertyToID("_Amount");
+
+    private void Awake() =>
+      _renderer = GetComponent<MeshRenderer>();
+
     public event Action OnDeath;
 
     private void Start() => 
@@ -30,11 +39,24 @@ namespace Enemy
     {
       health.HealthChanged -= OnHealthChanged;
       follow.enabled = false;
+      
+      _renderer.shadowCastingMode = ShadowCastingMode.Off;
+      StartCoroutine(DissolveBody(bodyDestructionDelay));
       Destroy(gameObject, bodyDestructionDelay);
       
-      //Animations and other staff
-      
+
       OnDeath?.Invoke();
+    }
+
+    private IEnumerator DissolveBody(float duration)
+    {
+      float progress = 0.0f;
+      while (progress <= duration)
+      {
+        progress += Time.deltaTime;
+        _renderer.material.SetFloat(DissolveAmount, Mathf.Clamp01(progress / duration));
+        yield return null;
+      }
     }
 
     private void Reset()
